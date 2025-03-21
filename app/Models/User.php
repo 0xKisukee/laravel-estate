5a868123b1ca596ecd86c14baa0ddc4d31656f7b
0xKisukee <aitbenali0502@gmail.com>
2025-03-22 00:27:02 +0100

    create user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function isOwner()
+    {
+        return $this->role === 'owner';
+    }
+
+    public function isTenant()
+    {
+        return $this->role === 'tenant';
+    }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->enum('role', ['owner', 'tenant']);
+            $table->string('first-name');
+            $table->string('last-name');
+            $table->string('phone');
+            $table->string('iban');
+            $table->string('bic');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
