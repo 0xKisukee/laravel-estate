253dec3e376c041bea5f590c3a6a17054ebe40f6
0xKisukee <aitbenali0502@gmail.com>
2025-03-22 18:37:16 +0100

    created payment factory

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Property;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment>
@@ -14,10 +17,30 @@ class PaymentFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    // This creates an upcoming payment
     public function definition(): array
     {
         return [
-            //
+            'amount' => random_int(0, 950),
+            'due_date' => fake()->dateTimeBetween('now', '+2 months')->format('Y-m-d'),
+            'paid_date' => null,
+            'property_id' => Property::all()->random()->id,
         ];
+    }
+
+    // This creates a paid payment
+    public function paid(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'paid_date' => fake()->dateTimeBetween('-1 months', 'today')->format('Y-m-d'),
+        ]);
+    }
+
+    // This creates a due payment
+    public function due(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'due_date' => fake()->dateTimeBetween('-2 months', '-1 month')->format('Y-m-d'),
+        ]);
     }
 }
