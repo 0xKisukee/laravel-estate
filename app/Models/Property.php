09593c4f8b6feee2b930a62ad7c4d5500560705e
0xKisukee <aitbenali0502@gmail.com>
2025-03-22 17:29:02 +0100

    enhanced property class

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -4,24 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Property extends Model
 {
     use HasFactory;
 
-    public function rented() {
-        if ($this->tenant_id == null) {
-            return false;
-        } else {
-            return true;
-        }
+    public function isRented(): bool {
+        return isset($this->tenant_id);
     }
 
-    public function owner() {
+    public function owner(): BelongsTo {
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function tenant() {
+    public function tenant(): BelongsTo {
         return $this->belongsTo(User::class, 'tenant_id');
     }
 }
