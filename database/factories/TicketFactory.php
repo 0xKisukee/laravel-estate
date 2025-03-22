f80570359a59158f45c0b500e4992ecb1977103f
0xKisukee <aitbenali0502@gmail.com>
2025-03-22 21:19:42 +0100

    add tickets files

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Property;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ticket>
+ */
+class TicketFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'type' => 'other',
+            'description' => 'Please repair the toilets.',
+            'status' => 'open',
+            'property_id' => Property::all()->random()->id,
+        ];
+    }
+}
---
+++ database/migrations/2025_03_22_193158_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['repair', 'payment', 'other'])->default('other');
+            $table->text('description');
+            $table->enum('status', ['open', 'closed'])->default('open');
+            $table->foreignId('property_id')->constrained('properties');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
