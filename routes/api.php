e0f1d6eca4677658080e7c39f93ff4bc0fa393b9
0xKisukee <aitbenali0502@gmail.com>
2025-03-23 00:34:55 +0100

    add register endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PropertyController;
+
+Route::post('/users', [UserController::class, 'register']);
 
 Route::get('/properties', [PropertyController::class, 'getAll']);
 Route::post('/properties', [PropertyController::class, 'store']);
