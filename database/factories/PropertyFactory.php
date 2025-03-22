9db698c3e931fe20185d9d1d59c3f20cd462b386
0xKisukee <aitbenali0502@gmail.com>
2025-03-22 21:18:50 +0100

    sexy

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -4,18 +4,12 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class PropertyFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
 
     /**
      * Define the model's default state.
@@ -32,27 +26,5 @@ class PropertyFactory extends Factory
             'owner_id' => User::where('role', 'owner')->inRandomOrder()->first()?->id,
             'tenant_id' => User::where('role', 'tenant')->inRandomOrder()->first()?->id,
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
-    public function owner(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'role' => 'owner',
-        ]);
-    }
-    public function tenant(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'role' => 'tenant',
-        ]);
     }
 }
