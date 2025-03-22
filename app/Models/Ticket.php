a051f952d6d5014e04d102363192bbbcdf73adca
0xKisukee <aitbenali0502@gmail.com>
2025-03-23 00:03:37 +0100

    get messages of a ticket

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
@@ -21,5 +22,9 @@ class Ticket extends Model
 
     public function tenant(): User {
         return $this->property->tenant;
+    }
+
+    public function messages(): HasMany {
+        return $this->hasMany(Message::class, 'ticket_id');
     }
 }
