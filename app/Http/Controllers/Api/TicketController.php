1f64e395d8c013af243872b3a8acaffbc479be56
0xKisukee <aitbenali0502@gmail.com>
2025-03-23 23:35:49 +0100

    new use case: create ticket

---
+++ app/Http/Controllers/Api/TicketController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\Property;
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreateTicketRequest;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateTicketRequest $request)
+    {
+        $property = Property::findOrFail($request->validated()['property_id']);
+
+        $ticket = Ticket::create([
+            'type' => $request->validated()['type'],
+            'description' => $request->validated()['description'],
+            'property_id' => $property->id,
+            'owner_id' => $property->owner_id,
+            'tenant_id' => $property->tenant_id,
+        ]);
+
+        return response()->json($ticket);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ticket $ticket)
+    {
+        if (!$ticket) {
+            return response()->json(['message' => 'Ticket not found'], 404);
+        }
+
+        return response()->json($ticket);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Ticket $ticket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ticket $ticket)
+    {
+        //
+    }
+
+    public function getTickets(User $user, Request $request) {
+        $userArray = (new UserResource($user))->toArray(request());
+        $tickets = $userArray['tickets'];
+
+        return response()->json($tickets);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PropertyController;
 use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\TicketController;
 
 Route::apiResource('users', UserController::class);
 Route::apiResource('properties', PropertyController::class);
+Route::apiResource('tickets', TicketController::class);
 
 // Owners can get the list of their properties
 Route::get('users/{user}/properties', [UserController::class, 'getOwnerProperties']);
@@ -14,7 +16,7 @@ Route::get('users/{user}/properties', [UserController::class, 'getOwnerPropertie
 // Tenants can get their property infos
 Route::get('users/{user}/property', [UserController::class, 'getTenantProperty']);
 
-// All users can get the list of their payments
+// Owners and tenants can get the list of their payments
 Route::get('users/{user}/payments', [UserController::class, 'getPayments']);
 
 // Owners can record a payment to set it as paid
@@ -25,3 +27,6 @@ Route::patch('properties/{property}/setTenant/{user}', [PropertyController::clas
 
 // Owners can set a tenant for their property
 Route::patch('properties/{property}/removeTenant', [PropertyController::class, 'removeTenant']);
+
+// Owners and tenants can get their tickets
+Route::get('user/{user}/tickets', [TicketController::class, 'getTickets']);
