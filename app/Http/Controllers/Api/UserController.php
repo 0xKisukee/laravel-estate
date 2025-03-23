44230e39705a2ebb9ccba96c1dc26cfb59a33590
0xKisukee <aitbenali0502@gmail.com>
2025-03-23 16:46:03 +0100

    use of FormRequest for user registration

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -11,27 +12,10 @@ use Illuminate\Support\Facades\Validator;
 class UserController extends Controller
 {
     // Create a user
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        // Validation
-        $request->validate([
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:8|confirmed', //'password_confirmation'
-        ]);
-
         // Creates a new user
-        $user = User::create([
-            'role' => $request->role,
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'phone' => $request->phone,
-            'iban' => $request->iban,
-            'bic' => $request->bic,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),  // Hash password
-        ]);
+        $user = User::create($request->validated());
 
         // Return response
         return response()->json([
