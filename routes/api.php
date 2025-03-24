70d3141210fc4af5a47f3adb79794b44f7f51423
0xKisukee <aitbenali0502@gmail.com>
2025-03-24 22:38:19 +0100

    routes cleanup

--- routes/api.php
+++ routes/api.php
@@ -16,33 +16,52 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 //Auth routes
-Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::middleware('auth:sanctum')->group(function () {
     // All users can log out
     Route::post('logout', [AuthController::class, 'logout']);
-    // All users can see their own infos
-    Route::get('users/{user}', [UserController::class, 'show']);
-    // Owners can create a property
-    Route::post('properties', [PropertyController::class, 'store']);
-    // Owners can delete a property TO DO
-    // Owners can edit a property TO DO
-    // All users can get their property infos
-    Route::get('properties/{property}', [PropertyController::class, 'show']);
-    // All users can get the list of their properties (returns a single property array for tenants)
-    Route::get('users/{user}/properties', [UserPropertyController::class, 'index']);
-    // All users can get the list of their payments
-    Route::get('users/{user}/payments', [UserPaymentController::class, 'index']);
+
+    Route::prefix('users/{user}')->group(function () {
+        // All users can see their own infos
+        Route::get('', [UserController::class, 'show']);
+        // All users can get the list of their properties (returns a single property array for tenants)
+        Route::get('properties', [UserPropertyController::class, 'index']);
+        // All users can get the list of their payments
+        Route::get('payments', [UserPaymentController::class, 'index']);
+        // All users can get their tickets
+        Route::get('tickets', [UserTicketController::class, 'index']);
+    });
+
+    Route::prefix('properties')->group(function () {
+        // Owners can create a property
+        Route::post('', [PropertyController::class, 'store']);
+        // All users can get their property infos
+        Route::get('{property}', [PropertyController::class, 'show']);
+
+        // I DON'T KNOW WHAT TO DO WITH THESE 2 ROUTES...
+        // Owners can set a tenant for a property
+        Route::patch('{property}/setTenant', [PropertyController::class, 'setTenant']);
+        // Owners can remove a tenant from a property
+        Route::patch('{property}/removeTenant', [PropertyController::class, 'removeTenant']);
+    });
+
     // Owners can record a payment to set it as paid
     Route::patch('payments/{payment}', [PaymentController::class, 'update']);
-    // All users can create and see a ticket, Owners can update a ticket
-    Route::apiResource('tickets', TicketController::class);
-    // All users can get their tickets
-    Route::get('users/{user}/tickets', [UserTicketController::class, 'index']);
+
     // All users can get a ticket's messages
     Route::get('tickets/{ticket}/messages', [TicketMessageController::class, 'index']);
     // All users can add a message to a ticket
     Route::post('tickets/{ticket}/messages', [TicketMessageController::class, 'store']);
-    // Owners can set a tenant for a property
-    Route::patch('properties/{property}/setTenant', [PropertyController::class, 'setTenant']);
-    // Owners can remove a tenant from a property
-    Route::patch('properties/{property}/removeTenant', [PropertyController::class, 'removeTenant']);
+
+    // All users can create and see a ticket, Owners can update a ticket
+    Route::apiResource('tickets', TicketController::class, [
+        'only' => ['store', 'show', 'update']
+    ]);
+
+
+
+
+    // Routes to implement
+    // Owners can delete a property TO DO
+    // Owners can edit a property TO DO
+
 });
