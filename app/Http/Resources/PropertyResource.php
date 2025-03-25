c660866daebed9925847a2f9b53cef5a2e5f55f0
0xKisukee <aitbenali0502@gmail.com>
2025-03-25 01:04:45 +0100

    create property resource (unused)

---
+++ app/Http/Resources/PropertyResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PropertyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $owner = $this->owner;
+        $tenant = $this->tenant;
+
+        return [
+            'id' => $this->id,
+            'type' => $this->role,
+            'rent' => $this->rent,
+            'city' => $this->city,
+            'address' => $this->address,
+
+            // Nested resources
+            'owner' => UserResource::collection($owner),
+            'tenant' => UserResource::collection($tenant),
+        ];
+    }
+}
